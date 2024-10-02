58ccc9ed817de094b9d7c017daf4cce0fe70446c
neroil <edwin.haffner@heig-vd.ch>
2024-10-02 11:35:48 +0200

    setup de la base

--- src/main/java/ch/heigvd/amt/Probe.java
+++ src/main/java/ch/heigvd/amt/Probe.java
@@ -1,5 +1,12 @@
 package ch.heigvd.amt;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Probe {
+
+    @Id
     private String url;
+
 }
--- src/main/java/ch/heigvd/amt/Status.java
+++ src/main/java/ch/heigvd/amt/Status.java
@@ -1,11 +1,22 @@
 package ch.heigvd.amt;
 
+import jakarta.persistence.*;
+
 import java.time.Duration;
 import java.time.Instant;
 
+//On doit ajouter des annotations pour que l'ORM reconnaisse les entités, utilisation de la réflexion !
+
+//Le status est une mesure de l'état d'une sonde à un instant donné
+@Entity
+//@Table(name = "Status")
 public class Status {
-    private int id;
+    @Id
+    private Long id;
+
+    @ManyToOne
     private Probe probe;
+
     private boolean isUp;
     private Instant timestamp;
     private Duration duration;
